e11235a376dd4e913cad5af2cf27fb08759f394e
Massacy <mayoi.dev2226@gmail.com>
2024-12-27 07:23:21 +0900

    fix 当月平日自動設定

--- replace_info.js
+++ replace_info.js
@@ -1,14 +1,13 @@
 const dayjs = require("dayjs");
+const weekday = require("dayjs/plugin/weekday");
 const jh = require("japanese-holidays");
 const executeTask = require("./executeTask");
 
 const days = []; // 当月の平日の日付を格納する配列
 const generateDate = () => {
-  let ymd = dayjs("2024-05").startOf("month").format("YYYY-MM-DD");
-  const ymdEnd = dayjs("2024-05")
-    .add(1, "M")
-    .startOf("month")
-    .format("YYYY-MM-DD");
+  const today = new Date();
+  let ymd = dayjs(today).startOf("month").format("YYYY-MM-DD");
+  const ymdEnd = dayjs(today).add(1, "M").startOf("month").format("YYYY-MM-DD");
   dayjs.extend(weekday);
   while (dayjs(ymd).isBefore(dayjs(ymdEnd))) {
     // 平日かつ祝日でない日付を配列に追加
@@ -28,5 +27,5 @@ console.log(days);
 
 for (const day of days) {
   console.log(day);
-  await executeTask(date);
+  executeTask(day);
 }
